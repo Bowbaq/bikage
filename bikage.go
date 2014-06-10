e07041ead961fc85514922e68e40b8d140aa7a57
Maxime Bury <maxime.bury@gmail.com>
2014-06-10 00:30:08 -0400

    Add Trip, Station structs

--- bikage.go
+++ bikage.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strconv"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,6 +17,20 @@ var (
 	username = flag.String("u", "", "citibike.com username")
 	password = flag.String("p", "", "citibike.com password")
 )
+
+type Station struct {
+	Id       string
+	Label    string
+	Lat, Lng float32
+}
+
+type Trip struct {
+	Id        string
+	StartId   string
+	StartTime time.Time
+	EndId     string
+	EndTime   time.Time
+}
 
 func main() {
 	flag.Parse()
@@ -68,9 +85,28 @@ func main() {
 	}
 
 	doc.Find("#tripTable .trip").Each(func(i int, tr *goquery.Selection) {
-		tr.Children().Each(func(i int, td *goquery.Selection) {
-			log.Println(td.Text())
-		})
-		log.Println("====================")
+		id, _ := tr.Attr("id")
+		start_id, _ := tr.Attr("data-start-station-id")
+		start_time, _ := parse_time(tr, "data-start-timestamp")
+		end_id, _ := tr.Attr("data-end-station-id")
+		end_time, _ := parse_time(tr, "data-end-timestamp")
+
+		trip := Trip{id, start_id, *start_time, end_id, *end_time}
+		log.Println(trip)
 	})
+}
+
+func parse_time(node *goquery.Selection, attr string) (*time.Time, error) {
+	time_str, ok := node.Attr(attr)
+	if !ok {
+		return nil, errors.New("attribute " + attr + " does not exist")
+	}
+
+	time_sec, err := strconv.ParseInt(time_str, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	time := time.Unix(time_sec, 0)
+	return &time, nil
 }
